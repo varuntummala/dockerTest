0f7514a6f32a62b410b6c2d82a9d6b1e43009e5d
Varun Tummala <varun.tummala@kelltontech.com>
2022-01-24 18:06:22 -0500

    Test for Auto Jenkins kick off

--- src/main/java/com/springBoot/userDetails/UserDetailsApplication.java
+++ src/main/java/com/springBoot/userDetails/UserDetailsApplication.java
@@ -13,7 +13,7 @@ public class UserDetailsApplication {
 
 		SpringApplication.run(UserDetailsApplication.class, args);
 	}
-
+//THis is test for Git Push and auto Jenkins kikc off
 	@Bean
 	@LoadBalanced
 	public RestTemplate restTemplate(){
